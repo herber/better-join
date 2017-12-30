8d4b6d94c6cc66197f65d02e906d7982704fd80a
Tobias Herber <40146720150275@litec.ac.at>
2017-12-30 14:40:20 +0100

    add more tests

--- test.js
+++ test.js
@@ -3,4 +3,7 @@ import join from '.';
 
 test('join strings', t => {
   t.is(join(['first', '2nd', '3rd', '4th', '5th', '6th']), 'first, 2nd, 3rd, 4th, 5th and 6th');
+  t.is(join([]), '');
+  t.is(join(['first', '2nd']), 'first and 2nd');
+  t.is(join(['first']), 'first');
 });
